9417712a1ebea837206fd5c91d8f114a128903a5
scorober <scorobers@gmail.com>
2019-12-15 18:42:41 -0800

    Added subfolders

---
+++ Systems/SpawnerSystem.cs
@@ -0,0 +1,31 @@
+﻿using GameHaven.Components;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+using MonoGame.Extended.Entities;
+using MonoGame.Extended.Entities.Systems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHaven.Systems
+{
+    class SpawnerSystem : EntityUpdateSystem
+    {
+
+        public SpawnerSystem() : base(Aspect.All(typeof(Transform2), typeof(Archer)))
+        {
+
+        }
+        public override void Initialize(IComponentMapperService mapperService)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
